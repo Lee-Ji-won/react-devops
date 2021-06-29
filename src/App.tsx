e9d8e0ec2f5234d3042ad7820f3c416e2fdad478
Lee-Ji-won <broken_x@naver.com>
2021-06-29 17:54:52 +0900

    Fix props msg

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          반갑습니다. <code>src/App.tsx</code> and save to reload.
+          테스트 완료. <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
